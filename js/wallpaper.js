02aae361584f913f4069dd6a7eb8e7cecdd19ef0
lichuang <1540956703@qq.com>
2025-02-06 15:08:47 +0800

    lc
    新增动态壁纸商店

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,22 +1,24 @@
-var wallpapers_videos = [
-    "https://img-baofun.zhhainiao.com/fs/scene/preview_video/a12b3e6a4b287b6a7f476b675b6265b1_preview.mp4",
-    "https://img-baofun.zhhainiao.com/pcwallpaper_ugc/preview/80477e49df9bf049a79f0dcd07caa3f1_preview.mp4",
-    "https://img-baofun.zhhainiao.com/fs/scene/preview_video/e08d7f125d2649c0d79db89c113b7bda_preview.mp4",
-    "https://img-baofun.zhhainiao.com/fs/scene/preview_video/990f574e543e2c8adddc10e353f7302c_preview.mp4",
-    "https://img-baofun.zhhainiao.com/fs/scene/preview_video/3dc9595f7d373c55cd62975a56326571_preview.mp4",
-    "https://img-baofun.zhhainiao.com/pcwallpaper_ugc/preview/03017b3429a4067c83bcedadb3f3e33a_preview.mp4",
-    "https://img-baofun.zhhainiao.com/pcwallpaper_ugc/scene/08b238fcf4b1365b453d44bb7d5063f1_preview.mp4",
-    "https://img-baofun.zhhainiao.com/pcwallpaper_ugc/preview/d8b5378e14c6d65d5a5741dfe0778e9d_preview.mp4",
-    "https://img-baofun.zhhainiao.com/pcwallpaper_ugc/scene/0e750a43cbad99045f3e749cc727e871_preview.mp4"
-];
-function changeWallpaper(url, isVideo) {
+function changeWallpaper(wallpaper) {
     let videoBGNode = document.getElementById('bg_video');
     let imgBGNode = document.getElementById('bg_img');
-    videoBGNode.hidden = !isVideo;
-    imgBGNode.hidden = isVideo;
-    if (isVideo) {
-        videoBGNode.setAttribute('src', url);
+    videoBGNode.hidden = !wallpaper['isVideo'];
+    imgBGNode.hidden = wallpaper['isVideo'];
+    if (wallpaper['isVideo']) {
+        videoBGNode.setAttribute('src', wallpaper['url']);
     }else{
-        imgBGNode.src=url;
+        imgBGNode.src=wallpaper['url'];
     }
+    window.localStorage.setItem('wallpaper',JSON.stringify(wallpaper));
 }
+window.addEventListener('message',(e)=>{
+    if (e.origin === window.location.origin) {
+        let event=e.data['event'];
+        switch(event)
+        {
+            case 'wallpaper':
+                let wallpaper=e.data['data'];
+                this.changeWallpaper(wallpaper);
+                ;break;
+        }     
+    }
+});
--- web/wallpaper/js/wallpaper.js
+++ web/wallpaper/js/wallpaper.js
@@ -1,3 +1,44 @@
-function purchase(wallpaperName) {
-    alert(`你已成功购买 ${wallpaperName}！`);
+var wallpapers=[];
+function applyWallpaper(idx)
+{
+    let msg=[];
+    msg['event']='wallpaper';
+    msg['data']=wallpapers[idx];
+    window.parent.postMessage(msg,'*');
 }
+function initStore()
+{
+    fetch('./store.json').then(response => {
+        // 检查响应状态是否为 200-299
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // 将响应数据解析为 JSON 格式
+        return response.json();
+    })
+   .then(data => {
+        // 处理解析后的 JSON 数据
+        wallpapers=data;
+        let store=document.getElementById('wallpaperstore');
+        for(let i=0;i<data.length;i++){
+            let wallpaper=data[i];
+            let cacheNode='';
+            if(wallpaper['isVideo'])
+            {
+                cacheNode='<div class="wallpaper">'+
+                '<video class="wallpaper-video" src="'+wallpaper['url']+'" autoplay="autoplay" loop="loop" muted="muted"/></video>'+ 
+                 '<button class="applybutton"  onclick='+'"applyWallpaper('+i+')">应用</button></div>';
+            }else{
+                cacheNode='<div class="wallpaper">'+
+                '<img class="wallpaper-img" src="'+wallpaper['url']+'"/>'+ 
+                 '<button class="applybutton" onclick='+'"applyWallpaper('+i+')">应用</button></div>';
+            }
+            store.insertAdjacentHTML('beforeend',cacheNode);
+        }
+    })
+   .catch(error => {
+        // 处理请求过程中发生的错误
+        console.error('Fetch error:', error);
+    });
+}
+this.initStore();
